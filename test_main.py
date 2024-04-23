f6176b13fef177f94e16591ff0928c90809181a2
Vladius3 <149819327+Vladius3@users.noreply.github.com>
2024-04-23 23:23:27 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,12 +5,18 @@ client = TestClient(app)
 
 
 def test_read_main():
+    '''
+    Основной тест
+    '''
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
 
 def test_predict_positive():
+    '''
+    Позитивный тест
+    '''
     response = client.post("/predict/",
                            json={"text": "I like machine learning!"})
     json_data = response.json()
@@ -19,6 +25,9 @@ def test_predict_positive():
 
 
 def test_predict_negative():
+    '''
+    Негативный тест
+    '''
     response = client.post("/predict/",
                            json={"text": "I hate machine learning!"})
     json_data = response.json()
