48dda560b029bafa35ba994f5319db9ddd7fc04c
Vladius3 <149819327+Vladius3@users.noreply.github.com>
2024-04-24 00:15:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,24 @@ classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
+    """
+    Обработчик для запроса GET на корневой URL.
+    
+    Returns:
+        dict: Словарь с сообщением "Hello World".
+    """
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
 def predict(item: Item):
+    """
+    Обработчик для запроса POST на /predict/.
+    
+    Args:
+        item (Item): Объект Item, содержащий текст для анализа настроения.
+        
+    Returns:
+        dict: Словарь с предсказанным настроением текста.
+    """
     return classifier(item.text)[0]
