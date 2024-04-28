2300002a0ca4c25c727552cefc7f862cccd304ca
Vladius3 <149819327+Vladius3@users.noreply.github.com>
2024-04-28 22:23:13 +0300

    Update main.py to flake8

--- main.py
+++ main.py
@@ -1,71 +1,36 @@
-rom fastapi import FastAPI
-
+from fastapi import FastAPI
 from transformers import pipeline
-
 from pydantic import BaseModel
 
 
-
-
-
 class Item(BaseModel):
-
     text: str
 
 
-
-
-
 app = FastAPI()
-
 classifier = pipeline("sentiment-analysis")
 
 
-
-
-
 @app.get("/")
-
 def root():
-
     """
-
     Обработчик для запроса GET на корневой URL.
 
-
-
     Returns:
-
         dict: Словарь с сообщением "Hello World".
-
     """
-
     return {"message": "Hello World"}
 
 
-
-
-
 @app.post("/predict/")
-
 def predict(item: Item):
-
     """
-
     Обработчик для запроса POST на /predict/.
 
-
-
     Args:
-
         item (Item): Объект Item, содержащий текст для анализа настроения.
 
-
-
     Returns:
-
         dict: Словарь с предсказанным настроением текста.
-
     """
-
     return classifier(item.text)[0]
